269d7e18a934a8492f49e1d1bab822265a477960
joshuaobare <jobare.obare@gmail.com>
2022-08-13 15:58:24 +0300

    converted fetch to async function

--- src/API.js
+++ src/API.js
@@ -1,14 +1,49 @@
+/* eslint-disable require-jsdoc */
 const key = "62d50fd38f48aba39355b8ae5a3ae053";
-let url = `http://api.openweathermap.org/data/2.5/weather?q=London&APPID=${key}`;
+let url;
 
 function urlLocator(x) {
   return (url = `http://api.openweathermap.org/data/2.5/weather?q=${x}&APPID=${key}`);
 }
 
-fetch(urlLocator("Nairobi"), { mode: "cors" })
+function jsonParser(x) {
+  const temp = x.main.temp;
+  const pressure = x.main.pressure;
+  const maxTemp = x.main.temp_max;
+  const minTemp = x.main.temp_min;
+  const name = x.name;
+  const country = x.sys.country;
+  const weather = x.weather;
+  const windSpeed = x.wind.speed;
+
+  console.log(
+    `Today's temperature is ${temp}, pressure is ${pressure}, maximum temperature is ${maxTemp}, minimum temprerature is ${minTemp}`
+  );
+
+  return {temp,pressure,maxTemp,minTemp,name,country,weather,windSpeed}
+}
+
+/* fetch(urlLocator("Nairobi"), { mode: "cors" })
   .then(function (response) {
     return response.json();
   })
   .then(function (response) {
-    console.log(response);
-  });
+    jsonParser(response);
+  })
+  .catch(function(err) {
+      alert(`There's an error: ${err}`)
+  }); */
+
+async function getData() {
+  try {
+    const response = await fetch(urlLocator("Nairobi"), { mode: "cors" });
+    response.json().then(function (response) {
+        console.log(response)
+      jsonParser(response);
+    });
+  } catch {
+    alert(`There's an error`);
+  }
+}
+
+getData();
